a4bc5da4de06a64f465de90b17cd5c4c5d89a0a1
DevTheGamer187 <devthegamer187@gmail.com>
2025-02-19 14:37:59 +0530

    Update queue_q7_lessTC.py

--- practical list/queue_q7_lessTC.py
+++ practical list/queue_q7_lessTC.py
@@ -1,51 +1,67 @@
 class Queue:
-    def __init__(self,max_cap):
-        self.queue_items=[None]
-        self.size=0
-        self.capacity=int(max_cap)
-        self.front=0
+    def __init__(self, max_cap):
+        self.queue_items = []  
+        self.size = 0
+        self.capacity = int(max_cap)
+        self.front = 0
 
     def enqueue(self, item):
-        if self.size==self.capacity:
+        if self.size == self.capacity:
             print("Queue is full")
-            return
         else:
             self.queue_items.append(item)
+            self.size += 1
 
     def dequeue(self):
         if self.is_empty():
             print("Queue is empty")
-            return
+            return None
         else:
-            print(self.queue_items[self.front])
-            for i in range(1,self.size):
-                self.queue_items[i-1]=self.queue_items[i]
-            self.size=-1
-            self.queue_items[self.size]=None
-            return
-        
-    def front(self):
-        if not self.is_empty():
+            item = self.queue_items[self.front]
+            self.front += 1
+            self.size -= 1
+            if self.front > len(self.queue_items) // 2:
+                self.queue_items = self.queue_items[self.front:]
+                self.front = 0
+            return item
+
+    def front_element(self):
+        if self.is_empty():
             print("Queue is empty")
+            return None
         else:
-            print(self.queue_items[self.front])
+            return self.queue_items[self.front]
 
     def is_empty(self):
-        return len(self.queue) == 0
+        return self.size == 0
 
-    def size(self):
-        return len(self.queue)
+    def get_size(self):
+        return self.size
 
-# Example usage
-q = Queue()
+    def display(self):
+        if self.is_empty():
+            print("Queue is empty")
+        else:
+            print("Queue:", self.queue_items[self.front:self.front + self.size])
+
+q = Queue(3)
 q.enqueue(10)
 q.enqueue(20)
 q.enqueue(30)
 
-print("Front element:", q.front())
-print("Queue size:", q.size())
+print("Front element:", q.front_element())
+print("Queue size:", q.get_size())  # Changed from q.size() to q.get_size()
+
+q.display()
 
 print("Dequeued:", q.dequeue())
-print("Queue size after dequeue:", q.size())
+print("Queue size after dequeue:", q.get_size())  # Changed here as well
+
+q.display()
 
 print("Is queue empty?", q.is_empty())
+
+q.enqueue(40)  
+print("Queue size after attempting to enqueue when full:", q.get_size())  # And here
+
+q.display()
